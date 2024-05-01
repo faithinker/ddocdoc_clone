f10e4f81141addb958d7c77c79cefbefadd1ae89
faithinker <faithinker12@gmail.com>
2024-05-01 14:52:47 +0900

    [네이버 지도][AOS] 빌드 오류 해결!!  - gradle.properties에서 RClass = false 추가  - MainActivity.kt  @NonNull 제거  - flutter_local_notifications 주석처리 > Pub 오류  - 로컬에 설치된 ndk 버전 명시

--- android/app/src/main/kotlin/com/joohyupkim/ddocdoc/ddocdoc_clone/MainActivity.kt
+++ android/app/src/main/kotlin/com/joohyupkim/ddocdoc/ddocdoc_clone/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.joohyupkim.ddocdoc.ddocdoc_clone
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
